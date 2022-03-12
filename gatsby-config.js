bcf43dc7a1c8201ee07a59bcce08576cbe60e513
Matteo Melotti <matteo.melotti@devinterface.com>
2022-03-12 16:22:35 +0100

    pwa config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Matteo Melotti Portfolio`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Matteo Melotti Portfolio`,
+        short_name: `MM Portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
